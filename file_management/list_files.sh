44bf69bf343b6bb0320db1bc4c2c260de5bd4c95
Husain Alshaikahmed <alshaikhahmed@hotmail.com>
2024-07-14 05:35:41 -0500

    added list_files script

---
+++ file_management/list_files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Display a message indicating what the script does
+echo "Listing all files in the current directory with detailed information:"
+
+# -l: use a long listing format
+# -h: with -l, print sizes in human readable format 
+# --color=auto: add color to the output
+ls -lh --color=auto
+
